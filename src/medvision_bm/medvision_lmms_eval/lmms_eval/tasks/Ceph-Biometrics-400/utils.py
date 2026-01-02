512e5854e71d35e426a5d34e12335539c45501d3
YongchengYao <yc.yao@ed.ac.uk>
2026-01-02 17:21:16 +0000

    [src,benchmark] feat(fix): new benchmark task, A/D tasks with CoT (add missing import in utils.py)

--- src/medvision_bm/medvision_lmms_eval/lmms_eval/tasks/Ceph-Biometrics-400/utils.py
+++ src/medvision_bm/medvision_lmms_eval/lmms_eval/tasks/Ceph-Biometrics-400/utils.py
@@ -1,15 +1,16 @@
-from medvision_ds.datasets.Ceph_Biometrics_400 import preprocess_biometry 
-
 from lmms_eval.tasks.medvision.medvision_utils import (
-    doc_to_visual,
-    doc_to_target_BiometricsFromLandmarks,
-    process_results_BiometricsFromLandmarks,
-    aggregate_results_MAE,
-    aggregate_results_MRE,
     aggregate_results_avgMAE,
     aggregate_results_avgMRE,
+    aggregate_results_MAE,
+    aggregate_results_MRE,
     aggregate_results_SuccessRate,
+    create_doc_to_text_BiometricsFromLandmarks,
+    create_doc_to_text_BiometricsFromLandmarks_CoT,
+    doc_to_target_BiometricsFromLandmarks,
+    doc_to_visual,
+    process_results_BiometricsFromLandmarks,
 )
-from lmms_eval.tasks.medvision.medvision_utils import create_doc_to_text_BiometricsFromLandmarks
+from medvision_ds.datasets.Ceph_Biometrics_400 import preprocess_biometry
 
 doc_to_text_BiometricsFromLandmarks = create_doc_to_text_BiometricsFromLandmarks(preprocess_biometry)
+doc_to_text_BiometricsFromLandmarks_CoT = create_doc_to_text_BiometricsFromLandmarks_CoT(preprocess_biometry)
--- src/medvision_bm/medvision_lmms_eval/lmms_eval/tasks/FeTA24/utils.py
+++ src/medvision_bm/medvision_lmms_eval/lmms_eval/tasks/FeTA24/utils.py
@@ -1,21 +1,29 @@
-from medvision_ds.datasets.FeTA24 import preprocess_detection, preprocess_biometry, preprocess_segmentation
-
 from lmms_eval.tasks.medvision.medvision_utils import (
-    doc_to_visual,
-    doc_to_target_BoxCoordinate,
-    doc_to_target_MaskSize,
-    doc_to_target_BiometricsFromLandmarks,
-    process_results_BoxCoordinate,
-    process_results_MaskSize,
-    process_results_BiometricsFromLandmarks,
-    aggregate_results_MAE,
-    aggregate_results_MRE,
     aggregate_results_avgMAE,
     aggregate_results_avgMRE,
+    aggregate_results_MAE,
+    aggregate_results_MRE,
     aggregate_results_SuccessRate,
+    create_doc_to_text_BiometricsFromLandmarks,
+    create_doc_to_text_BiometricsFromLandmarks_CoT,
+    create_doc_to_text_BoxCoordinate,
+    create_doc_to_text_MaskSize,
+    doc_to_target_BiometricsFromLandmarks,
+    doc_to_target_BoxCoordinate,
+    doc_to_target_MaskSize,
+    doc_to_visual,
+    process_results_BiometricsFromLandmarks,
+    process_results_BoxCoordinate,
+    process_results_MaskSize,
 )
-from lmms_eval.tasks.medvision.medvision_utils import create_doc_to_text_BoxCoordinate, create_doc_to_text_BiometricsFromLandmarks, create_doc_to_text_MaskSize
+from medvision_ds.datasets.FeTA24 import (
+    preprocess_biometry,
+    preprocess_detection,
+    preprocess_segmentation,
+)
 
 doc_to_text_BoxCoordinate = create_doc_to_text_BoxCoordinate(preprocess_detection)
 doc_to_text_BiometricsFromLandmarks = create_doc_to_text_BiometricsFromLandmarks(preprocess_biometry)
 doc_to_text_MaskSize = create_doc_to_text_MaskSize(preprocess_segmentation)
+
+doc_to_text_BiometricsFromLandmarks_CoT = create_doc_to_text_BiometricsFromLandmarks_CoT(preprocess_biometry)
